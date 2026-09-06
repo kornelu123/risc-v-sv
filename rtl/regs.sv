c98e7a07493634db9d9bc9ca640fc543a5dbdef6
kornelu123 <kornelu12@gmail.com>
2026-09-06 19:08:25 +0200

    Add basic RTL with basic tb

---
+++ rtl/regs.sv
@@ -0,0 +1,12 @@
+package regs;
+  typedef enum bit[4:0] {
+    ZERO, RA, SP, GP, TP,
+    T0, T1, T2, S0, S1, A10,
+    A1, A2, A3, A4, A5, A6, A7,
+    S2, S3, S4, S5, S6, S7, S8, S9,
+    S10, S11, T3, T4, T5, T6
+  } reg_t;
+
+  bit [31:0] reg_page [31:0];
+  bit [31:0] pc;
+endpackage
